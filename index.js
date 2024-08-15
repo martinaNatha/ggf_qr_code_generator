3524868c75bb17a03f62d11900fbb5405bc8cd04
martinaNatha <113536299+martinaNatha@users.noreply.github.com>
2024-08-15 10:06:39 -0400

    docker add

--- index.js
+++ index.js
@@ -528,7 +528,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // const server = http.createServer(app);
 
-app.set("port", process.env.PORT || 5000);
+app.set("port", process.env.PORT || 8085);
 
 server.listen(app.get("port"), () => {
   console.log("server on port", app.get("port"));
